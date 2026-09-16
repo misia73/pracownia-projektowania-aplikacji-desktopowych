5c33f44435bd8554099de75f08c518fb69563f31
wexxterz <mirvikyt@gmail.com>
2026-09-16 13:30:31 +0200

    okienko zrobione

--- 16092026/16092026/obj/Debug/net8.0-windows/16092026_b2yv1prl_wpftmp.AssemblyInfo.cs
+++ 16092026/16092026/obj/Debug/net8.0-windows/16092026_b2yv1prl_wpftmp.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("16092026")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090a5766dfb185bc5f1a2ad15f374243b08db024")]
 [assembly: System.Reflection.AssemblyProductAttribute("16092026")]
